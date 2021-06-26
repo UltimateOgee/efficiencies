a71a797dfe83f07b193d817b03c017378c93c8e7
espradli <espradli@trinity.edu>
2021-06-26 13:29:00 -0700

    get data from firebase

--- efficiencies-ui/src/App.js
+++ efficiencies-ui/src/App.js
@@ -2,16 +2,10 @@ import './App.css';
 import firebase from './Firebase.js'
 
 function App() {
-
-  const data = {
-    name: 'Los Angeles',
-    state: 'CA',
-    country: 'USA'
-  };
-  
-  // Add a new document in collection "cities" with ID 'LA'
-  var db = firebase.firestore();
-  const res = db.collection('teams').doc('teamId').set(data);
+  const snapshot = await db.collection('teams').get();
+  snapshot.forEach((doc) => {
+    console.log(doc.id, '=>', doc.data());
+  });
 
   return (
     <>
