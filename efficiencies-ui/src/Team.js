ee37e16f8cda5d6af394aa508439bb81188ee07f
Shubhankar Singh <shubhankarsingh@wta-mbp915.lan>
2021-08-01 21:53:00 -0700

    cleaned up comments in team.js

--- efficiencies-ui/src/Team.js
+++ efficiencies-ui/src/Team.js
@@ -3,65 +3,14 @@ import "firebase/firestore";
 import { useFirestoreDocData, useFirestore } from "reactfire";
 
 function Team() {
-  //object to hold form data:
-  /*const [teamData, setTeamData] = useState({
-    teamName: "",
-    ownerName: "",
-    contactEmail: "",
-    username: "",
-    password: "",
-  });*/
-
   // access a document in the firestore database
   const teamRef = useFirestore().collection("teams").doc("testTeam");
 
-  // subscribe to a document for realtime updates
-  // status: error, loading, success
-  // data: object from database
   const { status, data, firstValuePromise } = useFirestoreDocData(teamRef);
 
-  //update a doc
-  //delete data.thirdname;
-  //teamRef.set({ ...data, name: "examplename" });
-
-  // easily check the loading status
-  // if (status === "loading") {
-  //   return <p>Fetching team data...</p>;
-  // }
-
-  //FirebaseError: Function DocumentReference.set() called with invalid data. Data must be an object, but it was: undefined (found in document teams/testTeam)
-  //https://gitmemory.com/issue/FirebaseExtended/reactfire/320/756446884
-  /*
-  useEffect(async () => {
-    //TODO add status loading somewhere on form...
-    //await data being not undefined...
-    console.log(status);
-    console.log(firstValuePromise);
-    let myData = await firstValuePromise;
-    console.log(myData);
-    setTeamData(myData);
-  }, []);
-
-  //second param is optional list to link useEffect too
-  useEffect(() => {
-    teamRef.set(teamData);
-  }, [teamData, teamRef]);
-*/
-  /*
-  TEAM FORM ITEMS:
-    - Team name
-    - Owner name
-    - Contact Email
-    - Username
-    - Password
-  */
-
   const dataHandler = (event) => {
-    //setTeamData({ ...teamData, [event.target.name]: event.target.value });
     teamRef.set({ ...data, [event.target.name]: event.target.value });
   };
-
-  //const { teamName, ownerName, contactEmail, username, password } = data;
 
   return (
     <form>
