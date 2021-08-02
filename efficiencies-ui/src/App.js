5a249712ee0ddbbc4165d997c1f5dd7785c25906
Shubhankar Singh <shubhankarsingh@wta-mbp915.lan>
2021-08-01 23:03:28 -0700

    added basic auth and also router setup more complete

--- efficiencies-ui/src/App.js
+++ efficiencies-ui/src/App.js
@@ -1,4 +1,3 @@
-import Team from "./Team";
 import { FirebaseAppProvider } from "reactfire";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Login from "./main-components/Login";
@@ -7,6 +6,19 @@ import Analytics from "./main-components/Analytics";
 import Games from "./main-components/Games";
 import Profile from "./main-components/Profile";
 
+//are these imports correct?
+import "firebase/auth";
+import firebase from "firebase/app";
+import {
+  FirebaseAuthProvider,
+  FirebaseAuthConsumer,
+  IfFirebaseAuthed,
+  IfFirebaseAuthedAnd,
+  IfFirebaseUnAuthed,
+  IfFirebaseAuthedOr,
+} from "@react-firebase/auth";
+
+//move to own JS file
 const firebaseConfig = {
   apiKey: "AIzaSyAgcEnJzVuKthqlkwmaqUM0I_-0xmix_S4",
   authDomain: "efficiencies-e21b3.firebaseapp.com",
@@ -19,50 +31,61 @@ const firebaseConfig = {
 
 function App() {
   return (
-    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Login</Link>
-              </li>
-              <li>
-                <Link to="/newgame">New Game</Link>
-              </li>
-              <li>
-                <Link to="/analytics">Analytics</Link>
-              </li>
-              <li>
-                <Link to="/games">Games</Link>
-              </li>
-              <li>
-                <Link to="/profile">Profile</Link>
-              </li>
-            </ul>
-          </nav>
+    /*
+    <FirebaseAuthProver>
+      <Login>
+      <IfFireBaseAuthed>
+        <FirebaseAppProver>
+          <Router>
+    */
+    <FirebaseAuthProvider firebase={firebase} {...firebaseConfig}>
+      <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+        <IfFirebaseUnAuthed>
+          <Login />
+        </IfFirebaseUnAuthed>
+        <IfFirebaseAuthed>
+          <Router>
+            <div>
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/newgame">New Game</Link>
+                  </li>
+                  <li>
+                    <Link to="/analytics">Analytics</Link>
+                  </li>
+                  <li>
+                    <Link to="/games">Games</Link>
+                  </li>
+                  <li>
+                    <Link to="/profile">Profile</Link>
+                  </li>
+                </ul>
+              </nav>
 
-          <hr />
-          <Switch>
-            <Route exact path="/">
-              <Login />
-            </Route>
-            <Route path="/newgame">
-              <NewGame />
-            </Route>
-            <Route path="/analytics">
-              <Analytics />
-            </Route>
-            <Route path="/games">
-              <Games />
-            </Route>
-            <Route path="/profile">
-              <Profile />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    </FirebaseAppProvider>
+              <hr />
+              <Switch>
+                <Route exact path="/">
+                  <div>nothing here right now...</div>
+                </Route>
+                <Route path="/newgame">
+                  <NewGame />
+                </Route>
+                <Route path="/analytics">
+                  <Analytics />
+                </Route>
+                <Route path="/games">
+                  <Games />
+                </Route>
+                <Route path="/profile">
+                  <Profile />
+                </Route>
+              </Switch>
+            </div>
+          </Router>
+        </IfFirebaseAuthed>
+      </FirebaseAppProvider>
+    </FirebaseAuthProvider>
   );
 }
 
--- efficiencies-ui/src/main-components/Profile.js
+++ efficiencies-ui/src/main-components/Profile.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Team from "../Team";
+import firebase from "firebase";
 
 export default function Profile() {
   return (
@@ -7,6 +8,14 @@ export default function Profile() {
       <div>above team more stuff</div>
       <Team />
       <div>below team more stuff</div>
+      <div>Log Out:</div>
+      <button
+        onClick={() => {
+          firebase.auth().signOut();
+        }}
+      >
+        Sign Out
+      </button>
     </>
   );
 }
