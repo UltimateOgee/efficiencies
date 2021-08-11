8d622ed9eeefa94b727d65f86873d367c6ba8b36
Shubhankar Singh <shubhankarsingh@wta-mbp915.lan>
2021-08-10 22:36:37 -0700

    remove picker issue

--- efficiencies-ui/src/main-components/NewGame/Opponent.js
+++ efficiencies-ui/src/main-components/NewGame/Opponent.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import TextField from "@material-ui/core/TextField";
-import { DatePicker } from "@material-ui/pickers";
 
 export default function Opponent() {
+  const [date, setDate] = useState(new Date());
+
   return (
     <div>
       <form>
         <TextField id="outlined-basic" label="Opponent" />
+        <TextField
+          id="date"
+          label="Birthday"
+          type="date"
+          defaultValue={date}
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
       </form>
     </div>
   );
