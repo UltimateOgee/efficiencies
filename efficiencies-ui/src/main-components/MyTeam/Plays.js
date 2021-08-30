c6f064d386ceeedd15fbfe66fd42412a6633d292
espradli <espradli@trinity.edu>
2021-08-29 20:13:47 -0700

    Allow user to add plays with redux sync

--- efficiencies-ui/src/main-components/MyTeam/Plays.js
+++ efficiencies-ui/src/main-components/MyTeam/Plays.js
@@ -1,24 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Typography } from "@material-ui/core";
+import AddTableItem from "./AddTableItem";
+import { useSelector } from "react-redux";
 
 export default function Plays() {
+  // If playFields is changed, you must also change interface in RootReducer
+  const playFields = ['Name', 'Type', 'Possession'];
+  const [plays, setPlays] = useState([]);
+
+  const reduxPlays = useSelector(({ firebase: { profile } }) => profile.plays)
+  useEffect(() => {
+    setPlays(reduxPlays);
+  }, [reduxPlays])
+
+  // Potential table example - https://material-ui.com/components/tables/#ReactVirtualizedTable.js
   return (
     <>
       <Typography variant="h5">
-      My Plays
+      Plays
       </Typography>
+      
+      <Typography variant="h5">
+      Offense
+      </Typography>
+
       <table>
         <tr>
-          <th>Play Name</th>
-          <th>Play Type</th>
-          <th>Posesion</th>
+          {playFields.map(field => <th>{field}</th>)}
         </tr>
+        {plays ? plays.map(play => 
+          {if (play.Possession !== 'Defence') {
+            return(<tr>
+              {playFields.map(field => 
+                <td>{play[field] }</td>
+              )}
+            </tr>)
+          }else return null}
+          ) : 'loading...'}
+
+        <AddTableItem fields={playFields} type='plays'/>
+      </table>
+
+      <Typography variant="h5">
+      Defence
+      </Typography>
+
+      <table>
         <tr>
-          <td>Box and 1</td>
-          <td>Zone</td> 
-          <td>6'4"</td>
-          <td>190lbs</td>
+          {playFields.map(field => <th>{field}</th>)}
         </tr>
+        {plays ? plays.map(play => 
+          {if (play.Possession !== 'Offense') {
+            return(<tr>
+              {playFields.map(field => 
+                <td>{play[field] }</td>
+              )}
+            </tr>)
+          }else return null}
+          ) : 'loading...'}
+
+        <AddTableItem fields={playFields} type='plays'/>
       </table>
     </>
   );
