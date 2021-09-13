fa5610aa37e4bd78876658d4cda827a82234a4d7
espradli <espradli@trinity.edu>
2021-09-12 22:56:38 -0700

    Modularize bar chart and create efficiency table

--- efficiencies-ui/src/main-components/NewGame/BarChart.js
+++ efficiencies-ui/src/main-components/NewGame/BarChart.js
@@ -1,36 +1,52 @@
 import React, { useEffect } from "react";
 import Chart from "chart.js/auto";
 
-// https://www.chartjs.org/docs/latest/
-// https://blog.bitsrc.io/customizing-chart-js-in-react-2199fa81530a
-export default function BarChart() {
-  const labels = ['5-Out', 'Horns', '4-Out', 'Pick&Roll', 'Iso'];
-  const data = {
+/**
+ * Props object definition
+ * @typedef {Object} Props
+ * @property {string} id - ID for the bar chart
+ * @property {string[]} labels - Labels for each bar
+ * @property {string} title - Title for the bar chart
+ * @property {number[]} data - Data for each bar
+ * @property {string} color - color of the bars for the bar chart, in the for of 'rgb(r,g,b)'
+ */
+
+
+/**
+ * Creates a bar chart using the data passed in through props
+ * @param {Props} props 
+ */
+export default function BarChart({id, labels = [], title = '', data = [], color = 'rgb(201, 203, 207)'}) {
+  // https://www.chartjs.org/docs/latest/
+  // https://blog.bitsrc.io/customizing-chart-js-in-react-2199fa81530a
+  
+  const chartData = {
     labels: labels,
     datasets: [{
-      label: '2s',
-      data: [10, 8, 7, 2, 1],
-      backgroundColor: [
-        'rgba(201, 203, 207, 0.2)',
-        'rgba(201, 203, 207, 0.2)',
-        'rgba(201, 203, 207, 0.2)',
-        'rgba(201, 203, 207, 0.2)',
-        'rgba(201, 203, 207, 0.2)'
-      ],
-      borderColor: [
-        'rgb(201, 203, 207)',
-        'rgb(201, 203, 207)',
-        'rgb(201, 203, 207)',
-        'rgb(201, 203, 207)',
-        'rgb(201, 203, 207)'
-      ],
-      borderWidth: 1
+      label: title,
+      data: data,
+      backgroundColor: data.map(() => color)
+      // backgroundColor: [
+      //   'rgba(201, 203, 207, 0.2)',
+      //   'rgba(201, 203, 207, 0.2)',
+      //   'rgba(201, 203, 207, 0.2)',
+      //   'rgba(201, 203, 207, 0.2)',
+      //   'rgba(201, 203, 207, 0.2)'
+      // ],
+      // borderColor: [
+      //   'rgb(201, 203, 207)',
+      //   'rgb(201, 203, 207)',
+      //   'rgb(201, 203, 207)',
+      //   'rgb(201, 203, 207)',
+      //   'rgb(201, 203, 207)'
+      // ],
+      // borderWidth: 1
     }]
   };
 
   const config = {
     type: 'bar',
-    data: data,
+    data: chartData,
     options: {
       // scales: {
       //   y: {
@@ -41,14 +57,17 @@ export default function BarChart() {
   };
   
   useEffect(() => {
-    var ctx = document.getElementById('barChart');
-    const chart = new Chart(ctx, config);
+    var ctx = document.getElementById(id);
+    new Chart(ctx, config);
+    let div = document.getElementById(id + 'Wrapper');
+    div.style.width = '500px'
+    div.style.height = '300px'
   });
 
   return (
     <>
-      <div>
-          <canvas id="barChart" />
+      <div id={id + 'Wrapper'}>
+          <canvas id={id} />
       </div>
     </>
   );
